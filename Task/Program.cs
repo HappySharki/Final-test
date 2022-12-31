1f1a521615dcf36713c332ad3259f15a9ca7c787
HappyShark <shark-tex47@yandex.ru>
2022-12-31 19:47:00 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿string [] array = new string [5] {"hlo", "man", "iron", "black", "thor"};
+string [] temparray = new string [array.Length];
+int index = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        temparray[index] = array[i];
+        index += 1;
+    }
+}
+for (int j = 0; j < temparray.Length; j++)
+{
+    Console.Write($"{temparray[j]} ");
+}
